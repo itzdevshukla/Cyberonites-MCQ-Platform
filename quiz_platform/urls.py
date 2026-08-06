2ed572a3afaec27ed7329637efe4c5712e0c935e
Dev Shukla <devs44236@gmail.com>
2026-08-06 13:05:09 +0530

    Fix render import in urls.py for custom 404 and 500 error handlers

--- quiz_platform/urls.py
+++ quiz_platform/urls.py
@@ -5,7 +5,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.shortcuts import redirect
+from django.shortcuts import render, redirect
 
 
 from django.http import HttpResponse
